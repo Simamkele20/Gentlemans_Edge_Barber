174e572dea75144f982a1a6baa84f2a6c2dba28d
Simamkele20 <Princetancu06@gmail.com>
2024-03-05 10:17:49 +0200

    modelCont

--- Backend/model/Haircuts.js
+++ Backend/model/Haircuts.js
@@ -1,1 +1,76 @@
-// index
+import { connection as db } from "../config/index.js";
+class Haircuts {
+  fetchHaircuts(req, res) {
+    const qry = `
+        SELECT
+        cutID,cutName ,cutAmount,cutDescription,cutUrl,userID
+        FROM Haircuts
+        
+        `;
+    db.query(qry, (err, results) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        results,
+      });
+    });
+  }
+  fetchHaircut(req, res) {
+    const qry = `
+        cutID,cutName ,cutAmount,cutDescription,cutUrl,userID
+        FROM Haircuts
+        WHERE cutID=${req.params.id}
+        `;
+    db.query(qry, (err, result) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        result,
+      });
+    });
+  }
+  addHaircut(req, res) {
+    const qry = `INSERT INTO Haircuts SET ?;`;
+
+    db.query(qry, [req.body], (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg: "new haircut was added",
+      });
+    });
+  }
+
+  async updateHaircut(req, res) {
+    const data = req.body;
+    if (data?.userPwd) {
+      data.userPwd = await hash(data?.userPwd, 8);
+    }
+    const qry = `
+    UPDATE Haircuts 
+    SET ?
+    WHERE cutID=${req.params.id};`;
+    db.query(qry, [data], (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg: "Haircut updated",
+      });
+    });
+  }
+
+  deleteHaircut(req, res) {
+    const qry = `
+     DELETE FROM Haircuts
+     WHERE cutID=${req.params.id}
+  `;
+    db.query(qry, (err) => {
+      if (err) throw err;
+      res.json({
+        status: res.statusCode,
+        msg: "product deleted",
+      });
+    });
+  }
+}
+export { Haircuts };
--- Backend/model/index.js
+++ Backend/model/index.js
@@ -1,1 +1,8 @@
-// index
+import { Users } from "./Users.js";
+import { Haircuts } from "./Haircuts.js";
+import { Stylists } from "./Stylists.js";
+//create objects
+let users = new Users();
+let haircuts = new Haircuts();
+let stylists = new Stylists();
+export { users, haircuts, stylists };
