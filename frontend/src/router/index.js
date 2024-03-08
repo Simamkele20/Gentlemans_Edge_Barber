ed8e2536e6a5fc8af5946493617a80ad210ff5a7
Simamkele20 <Princetancu06@gmail.com>
2024-03-08 10:27:22 +0200

    FolderStructure

---
+++ frontend/src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from 'vue-router'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: ()=>import('@/views/HomeView.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import( '../views/AboutView.vue')
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: () => import( '../views/Products.vue')
+  },
+  {
+    path: '/product/:id',
+    name: 'product',
+    component: () => import( '../views/ProductView.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: () => import( '../views/AdminView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import( '../views/ContactView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import( '@/views/LoginView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
