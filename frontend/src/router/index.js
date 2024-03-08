463aa3f6f40466816db0a42532e14de31af80dd8
Simamkele20 <Princetancu06@gmail.com>
2024-03-08 11:22:43 +0200

    frontend

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,39 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: ()=>import('@/views/HomeView.vue')
+    component: HomeView
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import( '../views/AboutView.vue')
-  },
-  {
-    path: '/products',
-    name: 'products',
-    component: () => import( '../views/Products.vue')
-  },
-  {
-    path: '/product/:id',
-    name: 'product',
-    component: () => import( '../views/ProductView.vue')
-  },
-  {
-    path: '/admin',
-    name: 'admin',
-    component: () => import( '../views/AdminView.vue')
-  },
-  {
-    path: '/contact',
-    name: 'contact',
-    component: () => import( '../views/ContactView.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import( '@/views/LoginView.vue')
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
